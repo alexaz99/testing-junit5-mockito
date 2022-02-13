200b0cb494c31ea6e5c867b094c2fd593e7e44c7
Alex <azakotyansky@gmail.com>
2022-02-12 23:11:45 -0500

    Added mock tests

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -32,61 +32,42 @@ class VisitSDJpaServiceTest {
     @Test
     void findAll() {
         Visit visit = new Visit();
-
         Set<Visit> visits = new HashSet<>();
         visits.add(visit);
-
         when(visitRepository.findAll()).thenReturn(visits);
-
         Set<Visit> foundVisits = service.findAll();
-
         verify(visitRepository).findAll();
-
         assertThat(foundVisits).hasSize(1);
-
     }
 
     @Test
     void findById() {
         Visit visit = new Visit();
-
         when(visitRepository.findById(anyLong())).thenReturn(Optional.of(visit));
-
         Visit foundVisit = service.findById(1L);
-
         verify(visitRepository).findById(anyLong());
-
         assertThat(foundVisit).isNotNull();
     }
 
     @Test
     void save() {
         Visit visit = new Visit();
-
         when(visitRepository.save(any(Visit.class))).thenReturn(visit);
-
         Visit savedVisit = service.save(new Visit());
-
         verify(visitRepository).save(any(Visit.class));
-
         assertThat(savedVisit).isNotNull();
     }
 
     @Test
     void delete() {
         Visit visit = new Visit();
-
         service.delete(visit);
-
         verify(visitRepository).delete(any(Visit.class));
-
     }
 
     @Test
     void deleteById() {
-
         service.deleteById(1L);
-
         verify(visitRepository).deleteById(anyLong());
     }
 }
