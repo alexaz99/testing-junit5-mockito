2376b2f953be93fa3c353fce2d119eca600b0526
Alex <azakotyansky@gmail.com>
2022-03-29 18:27:16 -0400

    Added mock tests

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -16,6 +16,8 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 // run this test using MockitoExtension class
 @ExtendWith(MockitoExtension.class)
 class VisitSDJpaServiceTest {
@@ -34,15 +36,14 @@ class VisitSDJpaServiceTest {
 
         // 1. create a condition what to return when mocked object
         // visitRepository findAll method is called
-        when(visitRepository.findAll()).thenReturn(visits);
+        when(visitRepository.findAll()).thenReturn(visits); // need to set when method return something
 
         // 2. Execute the service method
         Set<Visit> foundVisits = visitSDJpaService.findAll();
 
         // 3. Should return 1 item in the collection
         assertTrue(foundVisits.size() == 1);
-
-        //assertTh
+        assertThat(foundVisits).isNotNull(); // use assertj API
 
         // 4. Validate that visitRepository.findAll() was called once
         verify(visitRepository).findAll();
@@ -51,21 +52,33 @@ class VisitSDJpaServiceTest {
     @Test
     void findById() {
         Visit visit = new Visit();
-        when(visitRepository.findById(anyLong())).thenReturn(Optional.of(visit));
+        when(visitRepository.findById(anyLong())).thenReturn(Optional.of(visit)); // need to set when method return something
         Visit foundVisit = visitSDJpaService.findById(1L);
         assertNotNull(foundVisit);
+        assertThat(foundVisit).isNotNull(); // use assertj API
         verify(visitRepository).findById(anyLong());
     }
 
     @Test
     void save() {
+        Visit visit = new Visit();
+        when(visitRepository.save(any(Visit.class))).thenReturn(visit); // need to set when method return something
+        Visit savedVisit = visitSDJpaService.save(visit);
+        assertNotNull(savedVisit);
+        assertThat(savedVisit).isNotNull(); // use assertj API
+        verify(visitRepository).save(any(Visit.class)); // make sure we call once
     }
 
     @Test
     void delete() {
+        Visit visit = new Visit();
+        visitSDJpaService.delete(visit);
+        verify(visitRepository).delete(any(Visit.class));
     }
 
     @Test
     void deleteById() {
+        visitSDJpaService.deleteById(1L);
+        verify(visitRepository).deleteById(anyLong());
     }
 }
