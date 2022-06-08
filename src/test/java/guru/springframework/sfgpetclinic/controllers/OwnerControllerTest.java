42c4a2e483b9533e92794f7ed6431713ebd51211
Alex <azakotyansky@gmail.com>
2022-06-08 15:52:21 -0400

    Added mock tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -4,20 +4,18 @@ import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.ArgumentMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
@@ -34,23 +32,60 @@ class OwnerControllerTest {
     @Mock
     BindingResult bindingResult;
 
+    @BeforeEach
+    void setUp() {
+        given(ownerService.findAllByLastNameLike(captor.capture())).willAnswer(invocation -> {
+
+            List<Owner> ownerList = new ArrayList<>();
+            String name = invocation.getArgument(0);
+            return null;
+        });
+    }
+
     @Test
     void processFindFormWildcardsString() {
         //given
         Owner owner = new Owner(1L, "Bob", "Black");
         List<Owner> ownerList = new ArrayList<>();
-        //final ArgumementCaptor
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // do
+        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
 
         //when
+        String viewName = ownerController.processFindForm(owner, bindingResult, null);
 
         //then
+        // Expecting %Black% and compare to captor value
+        org.assertj.core.api.Assertions.assertThat("%Black%").isEqualToIgnoringCase(captor.getValue());
+    }
+
+    @Captor
+    ArgumentCaptor<String> captor;
+
+
+    @Test
+    void processFindFormWildcardsStringAnnotation() {
+        //given
+        Owner owner = new Owner(1L, "Bob", "Black");
+        List<Owner> ownerList = new ArrayList<>();
+
+        // do agrument capture from this method
+        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+
+        //when
+        String viewName = ownerController.processFindForm(owner, bindingResult, null);
+
+        //then
+        // Expecting %Black% and compare to captor value
+        org.assertj.core.api.Assertions.assertThat("%Black%").isEqualToIgnoringCase(captor.getValue());
     }
 
     @Test
     void testProcessCreationFormHasErrors() {
         //given
         Owner owner = new Owner(1L, "Bob", "Tester");
-        BDDMockito.given(bindingResult.hasErrors()).willReturn(true);
+        given(bindingResult.hasErrors()).willReturn(true);
 
         //when
         String viewName = ownerController.processCreationForm(owner, bindingResult);
@@ -66,8 +101,8 @@ class OwnerControllerTest {
     void testProcessCreationFormNoErrors() {
         //given
         Owner owner = new Owner(5L, "Bob", "Tester");
-        BDDMockito.given(bindingResult.hasErrors()).willReturn(false);
-        BDDMockito.given(ownerService.save(any())).willReturn(owner);
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(ownerService.save(any())).willReturn(owner);
 
         //when
         String viewName = ownerController.processCreationForm(owner, bindingResult);
