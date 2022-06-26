b1010765969bad91299d4fef0c1b463b7f4c8a50
Alex <azakotyansky@gmail.com>
2022-06-25 21:55:25 -0400

    Added mock tests

---
+++ src/test/java/guru/springframework/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHello(){
+        System.out.println("TEST - Hello...");
+    }
+
+    @Test
+    public void testWorld() {
+        System.out.println("TEST - World!!!!");
+    }
+}
